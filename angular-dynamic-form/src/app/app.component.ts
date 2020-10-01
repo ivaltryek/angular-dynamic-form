bdf4d8047fb999608849d8f2747d388f21bfa577
Meet Vasani <meet.vasani86@gmail.com>
2020-10-01 14:04:40 +0530

    dynamic-forms added

--- angular-dynamic-form/src/app/app.component.ts
+++ angular-dynamic-form/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-dynamic-form';
+export class AppComponent implements OnInit {
+  public form: FormGroup;
+  ingredientarr: any;
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: [null],
+      ingredients: this.formBuilder.array([]),
+    });
+  }
+
+  addIngredients() {
+	const ingredientFormGroup = new FormGroup({
+		name: new FormControl(null),
+		amount: new FormControl(null),
+	  });
+    this.ingredientarr = this.form.get('ingredients');
+    
+	this.ingredientarr.push(ingredientFormGroup)
+  }
+
+  removeIngredient(i) {
+	  this.ingredientarr = this.form.get('ingredients');
+	  this.ingredientarr.removeAt(i);
+  }
+
+  onSubmit() {
+	alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.form.value, null, 4));
+  }
 }
